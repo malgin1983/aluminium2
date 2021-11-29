dccd01c4f396f6ae4cc5b96d1e996cb400b00995
malgin1983 <malghin@yandex.ru>
2021-11-30 02:58:06 +0300

    master - refactor started template

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [DatabaseModule, ProductsModule],
+    imports: [ConfigModule.forRoot(), DatabaseModule, ProductsModule],
     controllers: [AppController],
     providers: [AppService],
 })
